a7923c2c2a4fd36a29b55bd23818b15a346b5de6
sevalenciagr <sevalenciagr@gmail.com>
2023-03-11 15:31:31 -0500

    fetch colombian info

--- src/components/colombia.tsx
+++ src/components/colombia.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Colombia } from '../interfaces/colombia';
 
-export default function Colombia() {
+export default function GetColombianData() {
+  const [data, setData] = useState<Colombia | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api-colombia.com/api/v1/Country/Colombia');
+      const data = await response.json();
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Cargando...</div>;
+  }
   return (
     <div>
-      Colombia
+      <h1>{data.name}</h1>
+      <p>Código ISO: {data.isoCode}</p>
+      <p>Capital: {data.stateCapital}</p>
+      <p>Moneda: {data.currency}</p>
+      <p>Población: {data.population}</p>
+      <p>Idiomas: {data.languages.join(', ')}</p>
+      {/* Agrega más campos según los datos que quieras mostrar */}
     </div>
     
   )
---
+++ src/interfaces/colombia.ts
@@ -0,0 +1,19 @@
+export interface Colombia {
+    id:             number;
+    name:           string;
+    description:    string;
+    stateCapital:   string;
+    surface:        number;
+    population:     number;
+    languages:      string[];
+    timeZone:       string;
+    currency:       string;
+    currencyCode:   string;
+    isoCode:        string;
+    internetDomain: string;
+    phonePrefix:    string;
+    radioPrefix:    string;
+    aircraftPrefix: string;
+    departments:    null;
+    presidents:     null;
+}
