78f50e009df6e02f60922a0ae50ba13845e56853
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-03-13 12:47:35 -0500

    Hamburguer menu navbar

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,49 +1,38 @@
 import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <Navbar bg="body-tertiary" expand="lg">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <Navbar.Brand href="#">
           <img
             src="https://www.svgrepo.com/show/242329/colombia.svg"
             alt="Colombia"
             style={{ width: "80px", height: "50px" }}
           />
-        </a>
-        <ul className="navbar-nav mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a
-              className="nav-link fw-bolder fs-4"
-              aria-current="page"
-              href="#"
-            >
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" className="fw-bolder fs-4">
               Colombia
-            </a>
-          </li>
-          <li className="nav-item">
-            <Link to="/departments" className="nav-link fw-bolder fs-4">
+            </Nav.Link>
+            <Nav.Link as={Link} to="/departments" className="fw-bolder fs-4">
               Departments
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/cities" className="nav-link fw-bolder fs-4">
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cities" className="fw-bolder fs-4">
               Cities and Towns
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/tourists" className="nav-link fw-bolder fs-4">
+            </Nav.Link>
+            <Nav.Link as={Link} to="/tourists" className="fw-bolder fs-4">
               Tourists
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/presidents" className="nav-link fw-bolder fs-4">
+            </Nav.Link>
+            <Nav.Link as={Link} to="/presidents" className="fw-bolder fs-4">
               Presidents
-            </Link>
-          </li>
-        </ul>
-        
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </div>
-    </nav>
+    </Navbar>
   );
 }
